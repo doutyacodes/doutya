d0d75646eea2c46b4526aa69455d688284cf49aa
avnigoyal25 <500090996@stu.upes.ac.in>
2024-08-16 21:27:22 +0530

    AI integrated

--- app/_services/GlobalApi.js
+++ app/_services/GlobalApi.js
@@ -1,12 +1,12 @@
-const {default:axios} =require('axios')
+const { default: axios } = require('axios')
 
-const CreateNewUser=(data)=>axios.post('/api/user',data)
+const CreateNewUser = (data) => axios.post('/api/user', data)
 const LoginUser = (data) => axios.post('/api/login', data);
 const GetUser = (token) => axios.get('/api/getUser', {
-    headers: {
-      'Authorization': `Bearer ${token}`
-    }
-  });
+  headers: {
+    'Authorization': `Bearer ${token}`
+  }
+});
 
 const GetQuizData = (id, token) => {
 
@@ -17,9 +17,16 @@ const GetQuizData = (id, token) => {
   });
 };
 
-export default{
-    CreateNewUser,
-    LoginUser,
-    GetUser,
-    GetQuizData
+const InterestResult = (data) => axios.post('/api/resultTwo', data, {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default {
+  CreateNewUser,
+  LoginUser,
+  GetUser,
+  GetQuizData,
+  InterestResult
 }
