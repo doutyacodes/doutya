15572c0aeb3712597671d723b2d1c31772a90bb7
unknown <jinojino122@gmai.com>
2024-08-20 18:03:24 +0530

    Dashboard Page

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,21 +1,21 @@
-export default {
-    schema: "./utils/schema.js",
-    dialect: 'mysql',
-    dbCredentials: {
-        host: "localhost",
-        user: "root",
-        database: "doutya",
-        password: 'jinoJINO'
-    }
-};
-
 // export default {
 //     schema: "./utils/schema.js",
 //     dialect: 'mysql',
 //     dbCredentials: {
-//         host: "68.178.163.247",
-//         user: "devusr_wowfyuser",
-//         database: "devusr_quiz_project",
-//         password: '###Wowfy123'
+//         host: "localhost",
+//         user: "root",
+//         database: "doutya",
+//         password: 'jinoJINO'
 //     }
 // };
+
+export default {
+    schema: "./utils/schema.js",
+    dialect: 'mysql',
+    dbCredentials: {
+        host: "68.178.163.247",
+        user: "devusr_wowfyuser",
+        database: "devusr_quiz_project",
+        password: '###Wowfy123'
+    }
+};
