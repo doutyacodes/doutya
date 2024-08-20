c821dcb411bdb19c21cdeb58b661bf782d212421
Himank Saini <himank.saini@doutya.com>
2024-08-20 20:32:24 +0530

    updated UI for both mobile and Web

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,21 +1,21 @@
-// export default {
-//     schema: "./utils/schema.js",
-//     dialect: 'mysql',
-//     dbCredentials: {
-//         host: "localhost",
-//         user: "root",
-//         database: "doutya",
-//         password: 'jinoJINO'
-//     }
-// };
-
 export default {
     schema: "./utils/schema.js",
     dialect: 'mysql',
     dbCredentials: {
-        host: "68.178.163.247",
-        user: "devusr_wowfyuser",
-        database: "devusr_quiz_project",
-        password: '###Wowfy123'
+        host: "localhost",
+        user: "root",
+        database: "doutya1",
+        password: 'Rambeer#2387'
     }
 };
+
+// export default {
+//     schema: "./utils/schema.js",
+//     dialect: 'mysql',
+//     dbCredentials: {
+//         host: "68.178.163.247",
+//         user: "devusr_wowfyuser",
+//         database: "devusr_quiz_project",
+//         password: '###Wowfy123'
+//     }
+// };
--- utils/index.js
+++ utils/index.js
@@ -1,20 +1,20 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
-// const connection = await mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   database: "doutya",
-//   password:'jinoJINO',
-//   port:'3306'
-// });
-
 const connection = await mysql.createConnection({
-  host: "68.178.163.247",
-  user: "devusr_wowfyuser",
-  database: "devusr_quiz_project",
-  password:'###Wowfy123',
+  host: "localhost",
+  user: "root",
+  database: "doutya1",
+  password:'Rambeer#2387',
   port:'3306'
 });
+
+// const connection = await mysql.createConnection({
+//   host: "68.178.163.247",
+//   user: "devusr_wowfyuser",
+//   database: "devusr_quiz_project",
+//   password:'###Wowfy123',
+//   port:'3306'
+// });
 
 export const db = drizzle(connection);
