ed8c8c33551f5de2c434a0386878cfec30b3ac0b
avnigoyal25 <500090996@stu.upes.ac.in>
2024-08-21 22:14:58 +0530

    further improvements

--- app/_services/GlobalApi.js
+++ app/_services/GlobalApi.js
@@ -74,9 +74,12 @@ const GetDashboarCheck = (token) => {
   });
 };
 
-const GetResult2=(token)=>axios.get('/api/getresult2',{
+const GetResult2=(token,countryParams)=>axios.get('/api/getresult2',{
   headers: {
     Authorization: `Bearer ${token}`,
+  },
+  params: {
+    country: countryParams, // Include countryParams in the query parameters
   }
 });
 
