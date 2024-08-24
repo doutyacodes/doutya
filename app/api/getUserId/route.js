96af437b27bce9e184575e65893478361e7423aa
shabeer_sf <shabeermen@gmail.com>
2024-08-24 10:02:04 +0530

    fixed userId issues

--- app/api/getUserId/route.js
+++ app/api/getUserId/route.js
@@ -1,34 +1,39 @@
-import { NextResponse } from 'next/server';
-import { authenticate } from '@/lib/jwtMiddleware';
-import { QUIZ_SEQUENCES } from '@/utils/schema';
-import { db } from '@/utils';
-import { eq } from 'drizzle-orm';
-import { RESULTS1 } from '@/utils/schema';
+import { NextResponse } from "next/server";
+import { authenticate } from "@/lib/jwtMiddleware";
+import { QUIZ_SEQUENCES } from "@/utils/schema";
+import { db } from "@/utils";
+import { eq } from "drizzle-orm";
+import { RESULTS1 } from "@/utils/schema";
 
-export async function GET(req)
-{
-    console.log('got user id function')
-    const authResult = await authenticate(req);
-    if (!authResult.authenticated) {
-        return authResult.response;
-      }
+export async function GET(req) {
+  console.log("got user id function");
+  const authResult = await authenticate(req);
+  if (!authResult.authenticated) {
+    return authResult.response;
+  }
 
-    const userData = authResult.decoded_Data;
-    
-    const userId = userData.userId;
-    
-    const type_sequences = await db.select({
-      typeSequence: QUIZ_SEQUENCES.type_sequence
-          }).from(QUIZ_SEQUENCES)
-          .where(eq(QUIZ_SEQUENCES.user_id, userId))
-          .where(eq(QUIZ_SEQUENCES.quiz_id, 1))
-          .execute();
+  const userData = authResult.decoded_Data;
 
-    console.log(type_sequences)
+  const userId = userData.userId;
 
-    const type=type_sequences[0].typeSequence
+  const type_sequences = await db
+    .select({
+      typeSequence: QUIZ_SEQUENCES.type_sequence,
+    })
+    .from(QUIZ_SEQUENCES)
+    .where(
+      and(eq(QUIZ_SEQUENCES.user_id, userId), eq(QUIZ_SEQUENCES.quiz_id, 1))
+    )
+    .execute();
 
-    const results = await db.select().from(RESULTS1).where(eq(RESULTS1.type_sequence, type));
+  console.log(type_sequences);
 
-    return NextResponse.json(results);
+  const type = type_sequences[0].typeSequence;
+
+  const results = await db
+    .select()
+    .from(RESULTS1)
+    .where(eq(RESULTS1.type_sequence, type));
+
+  return NextResponse.json(results);
 }
