546ec2a1c83d95f0cd0df2c28e934cedf0af861e
Himank Saini <himank.saini@doutya.com>
2024-09-02 10:44:35 +0530

    Updated UI for Quiz 2

--- app/CareerQuizSection/[taskId]/page.js
+++ app/CareerQuizSection/[taskId]/page.js
@@ -1,198 +1,192 @@
-"use client"
-import LoadingOverlay from '@/app/_components/LoadingOverlay';
-import QuizProgressAlert from '@/app/_components/QuizProgressAlert';
-import GlobalApi from '@/app/_services/GlobalApi';
+"use client";
+import LoadingOverlay from "@/app/_components/LoadingOverlay";
+import QuizProgressAlert from "@/app/_components/QuizProgressAlert";
+import GlobalApi from "@/app/_services/GlobalApi";
 // import { Toaster } from '@/components/ui/toaster';
-import { useRouter } from 'next/navigation';
-import React, { useState, useEffect } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 function Page({ params }) {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedChoice, setSelectedChoice] = useState(null);
-  const [quizCompleted, setQuizCompleted] = useState(false)
+  const [quizCompleted, setQuizCompleted] = useState(false);
   const [secondsRemaining, setSecondsRemaining] = useState(5);
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState([]);
   const [choices, setChoices] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
   const router = useRouter();
   const quizId = params.taskId;
-  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
 
   useEffect(() => {
-    const authCheck = ()=>{
-      if (typeof window !== 'undefined') {
+    const authCheck = () => {
+      if (typeof window !== "undefined") {
         const token = localStorage.getItem("token");
-        if(!token){
-          router.push('/login');
-          setIsAuthenticated(false)
-        }else{
-          setIsAuthenticated(true)
+        if (!token) {
+          router.push("/login");
+          setIsAuthenticated(false);
+        } else {
+          setIsAuthenticated(true);
         }
       }
     };
-    authCheck()
+    authCheck();
   }, [router]);
 
+  useEffect(() => {
+    const getQuizData = async () => {
+      setIsLoading(true);
+      try {
+        const token =
+          typeof window !== "undefined" ? localStorage.getItem("token") : null;
+        const resp = await GlobalApi.GetCareerQuiz(quizId, token);
+        setQuestions(resp.data.questions);
+        setChoices(resp.data.choices);
+        setCurrentQuestionIndex(resp.data.quizProgress);
 
-    useEffect(() =>{
-        const getQuizData = async()=>{
-            setIsLoading(true)
-            try {
-                const token = typeof window !== 'undefined' ? localStorage.getItem("token") : null;
-                const resp = await GlobalApi.GetCareerQuiz(quizId, token);
-                setQuestions(resp.data.questions); 
-                setChoices(resp.data.choices);
-                setCurrentQuestionIndex(resp.data.quizProgress);
-
-                if (resp.data.quizProgress > 0) {
-                  setShowAlert(true);  // Set showAlert to true when resuming the quiz
-                }
-
-              } catch (error) {
-                console.error('Error Fetching GetQuizData data:', error);
-              }finally {
-                  setIsLoading(false);
-              }
+        if (resp.data.quizProgress > 0) {
+          setShowAlert(true); // Set showAlert to true when resuming the quiz
         }
-        getQuizData()
-    }, [])
-
-    useEffect(() => {
-        if (quizCompleted) {
-            // setIsLoading(true)
-            const interval = setInterval(() => {
-                setSecondsRemaining((prevSeconds) => prevSeconds - 1);
-            }, 1000);
-        
-            const timer = setTimeout(() => {
-                router.replace('/dashboard'); 
-                console.log("Route");
-            }, 5000);
-        
-            return () => {
-                clearInterval(interval); 
-                clearTimeout(timer); 
-            };
-        }
-    }, [quizCompleted, router]);
-
-    const handleChoiceSelect = (choice) => {      
-        setSelectedChoice(choice);
-    };
-
-    const handleNext = () => {
-
-      const answer = 
-                { questionId: questions[currentQuestionIndex].questionId,
-                  optionId: selectedChoice.choiceId,
-                  optionText: selectedChoice.choiceText,
-                  personaTypeId: questions[currentQuestionIndex].personaTypeId
-                }
-      quizProgressSubmit(answer); 
-
-      if (currentQuestionIndex < questions.length - 1) {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-        setSelectedChoice(null); // Resetting selected choice for the next question
-      } else {
-
-        setQuizCompleted(true);      
-        quizSubmit()// Quiz finished, send data to API
+      } catch (error) {
+        console.error("Error Fetching GetQuizData data:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
+    getQuizData();
+  }, []);
 
-    const quizProgressSubmit = async (data) => {
-    
-        try {
-          const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
-          const resp = await GlobalApi.SaveCarrierQuizProgress(data, token, quizId);
-      
-          if (resp && resp.status === 201) {
-            console.log("Response:", resp.data);
-          } else {
-            console.error("Failed to save progress. Status code:", resp.status);
-            toast.error("There was a problem saving your progress. Please check your internet connection.");
-          }
-        } catch (error) {
-          console.error("Error submitting progress:", error.message);
-          toast.error("There was an error saving your progress. Please try again later.");
-        }
-    };
-    
-    const quizSubmit = async () => {
-      setIsLoading(true);
-      const token = typeof window !== 'undefined' ? localStorage.getItem("token") : null;
-        try {
-          const resp = await GlobalApi.SaveCareerQuizResult(token);
-          if (resp && resp.status === 201) {
-            toast.success('Quiz Completed successfully!');
-          } else {
-            toast.error('Failed to submit quiz.');
-            // alert('Failed Submitted results');
-          }
-        } catch (error) {
-          console.error('Error submitting quiz', error);
-          // toast.error('Error: Failed to create Challenge.');
-          toast.error('Error Error: Failed to submit quiz.');
-        } finally {
-          setIsLoading(false);
-        }
+  useEffect(() => {
+    if (quizCompleted) {
+      // setIsLoading(true)
+      const interval = setInterval(() => {
+        setSecondsRemaining((prevSeconds) => prevSeconds - 1);
+      }, 1000);
+
+      const timer = setTimeout(() => {
+        router.replace("/dashboard");
+        console.log("Route");
+      }, 5000);
+
+      return () => {
+        clearInterval(interval);
+        clearTimeout(timer);
+      };
     }
+  }, [quizCompleted, router]);
 
-  if(isLoading || !isAuthenticated){
-    return (
-        <div className='h-screen flex items-center justify-center text-white'>
-            <div>
-                <div className='font-semibold'>
-                     <LoadingOverlay loadText={"Loading..."}/>
-                </div>
-            </div>
-        </div>
-    )
-  }
+  const handleChoiceSelect = (choice) => {
+    setSelectedChoice(choice);
+  };
 
-  if (quizCompleted) {
+  const handleNext = () => {
+    const answer = {
+      questionId: questions[currentQuestionIndex].questionId,
+      optionId: selectedChoice.choiceId,
+      optionText: selectedChoice.choiceText,
+      personaTypeId: questions[currentQuestionIndex].personaTypeId,
+    };
+    quizProgressSubmit(answer);
+
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setSelectedChoice(null); // Resetting selected choice for the next question
+    } else {
+      setQuizCompleted(true);
+      quizSubmit(); // Quiz finished, send data to API
+    }
+  };
+
+  const quizProgressSubmit = async (data) => {
+    try {
+      const token =
+        typeof window !== "undefined" ? localStorage.getItem("token") : null;
+      const resp = await GlobalApi.SaveCarrierQuizProgress(data, token, quizId);
+
+      if (resp && resp.status === 201) {
+        console.log("Response:", resp.data);
+      } else {
+        console.error("Failed to save progress. Status code:", resp.status);
+        toast.error(
+          "There was a problem saving your progress. Please check your internet connection."
+        );
+      }
+    } catch (error) {
+      console.error("Error submitting progress:", error.message);
+      toast.error(
+        "There was an error saving your progress. Please try again later."
+      );
+    }
+  };
+
+  const quizSubmit = async () => {
+    setIsLoading(true);
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("token") : null;
+    try {
+      const resp = await GlobalApi.SaveCareerQuizResult(token);
+      if (resp && resp.status === 201) {
+        toast.success("Quiz Completed successfully!");
+      } else {
+        toast.error("Failed to submit quiz.");
+        // alert('Failed Submitted results');
+      }
+    } catch (error) {
+      console.error("Error submitting quiz", error);
+      // toast.error('Error: Failed to create Challenge.');
+      toast.error("Error Error: Failed to submit quiz.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading || !isAuthenticated) {
     return (
-      <div className='h-screen flex items-center justify-center text-white text-center'>
+      <div className="h-screen flex items-center justify-center text-white">
         <div>
-            <div className='text-4xl font-semibold'>
-              Quiz Completed successfully
-            </div>
-
-            <p className='mt-4'>
-                Navigating to the Home page in {secondsRemaining} seconds
-            </p>
-            
+          <div className="font-semibold">
+            <LoadingOverlay loadText={"Loading..."} />
+          </div>
         </div>
       </div>
     );
   }
-  
+
+  if (quizCompleted) {
+    return (
+      <div className="h-screen flex items-center justify-center text-white text-center">
+        <div>
+          <div className="text-4xl font-semibold">
+            Quiz Completed successfully
+          </div>
+
+          <p className="mt-4">
+            Navigating to the Home page in {secondsRemaining} seconds
+          </p>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div className='h-screen'>
-      <Toaster
-      position="top-center"
-      reverseOrder={false}
-      />
+    <div className="h-screen">
+      <Toaster position="top-center" reverseOrder={false} />
 
-      {
-        showAlert && (
-          <QuizProgressAlert />
-        )
-      }
-      {
-        questions.length > 0 &&
-        <div className='flex w-4/5 flex-col gap-8 justify-center items-center mx-auto py-4 border-solid border-4 text-white rounded-2xl'>
-        <div>
-          <p className='font-semibold text-4xl'>Question {currentQuestionIndex + 1}</p>
-        </div>
-        <div>
-          <p className='font-normal p-2 text-xl md:text-3xl'>{questions[currentQuestionIndex].questionText}</p>
-        </div>
+      {showAlert && <QuizProgressAlert />}
+      {questions.length > 0 && (
+        <div className="mt-4 pt-5 flex w-4/5 flex-col gap-8 justify-center items-center mx-auto py-4  text-white rounded-2xl">
+          <div>
+            <p className="font">{currentQuestionIndex + 1}/30</p>
+          </div>
+          <div>
+            <p className="font-bold p-2 text-xl md:text-3xl">
+              {questions[currentQuestionIndex].questionText}
+            </p>
+          </div>
 
-        {/* <div className='flex flex-col gap-2 w-full text-white'>
+          {/* <div className='flex flex-col gap-2 w-full text-white'>
           {questions[currentQuestionIndex]?.choices.map((choice, index) => (
             <button
               key={index}
@@ -204,11 +198,16 @@ function Page({ params }) {
           ))}
         </div> */}
 
-          <div className='flex flex-col gap-2 w-full text-white'>
+          <div className="flex flex-col gap-2 w-full text-white">
             {choices.map((choice, index) => (
               <button
                 key={index}
-                className={`py-2 px-4 ${selectedChoice?.choiceId === choice.choiceId ? 'bg-green-500' : 'bg-slate-400'}`}
+                className={`py-2 px-4 rounded-md hover:cursor-pointer
+                  hover:bg-purple-300 hover:text-black transition duration-300 ease-in-out ${
+                    selectedChoice?.choiceId === choice.choiceId
+                      ? "bg-green-500"
+                      : "bg-slate-400"
+                  }`}
                 onClick={() => handleChoiceSelect(choice)}
               >
                 {choice.choiceText}
@@ -216,17 +215,19 @@ function Page({ params }) {
             ))}
           </div>
 
-        <div>
-          <button
-            className={`bg-green-600 py-2 px-5 rounded-lg text-white ${selectedChoice ? '' : 'opacity-50 cursor-not-allowed'}`}
-            onClick={handleNext}
-            disabled={!selectedChoice}
-          >
-            Next
-          </button>
+          <div>
+            <button
+              className={`bg-green-600 py-2 px-5 rounded-lg text-white ${
+                selectedChoice ? "" : "opacity-50 cursor-not-allowed"
+              }`}
+              onClick={handleNext}
+              disabled={!selectedChoice}
+            >
+              Next
+            </button>
+          </div>
         </div>
-        </div>
-      }
+      )}
     </div>
   );
 }
