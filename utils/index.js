ae82f028de7a8abe8caeb3169e2a4ea72d22c45e
Mrunu22 <mrunalsoshte22@doutya.com>
2024-09-06 16:29:57 +0530

    db

--- utils/index.js
+++ utils/index.js
@@ -4,7 +4,7 @@ import mysql from "mysql2/promise";
 // const connection = await mysql.createConnection({
 //   host: "localhost",
 //   user: "root",
-//   database: "project1",
+//   database: "doutyadatabase",
 //   password:'',
 //   port:'3306'
 // });
