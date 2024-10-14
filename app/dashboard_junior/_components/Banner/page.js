672101a670c36ccf14abcfb71f6cd20e1eb3749d
shristigupta12 <shristiguptawork@gmail.com>
2024-10-14 13:15:12 +0530

    disabled test 1 feature in mobile view

--- app/dashboard_junior/_components/Banner/page.js
+++ app/dashboard_junior/_components/Banner/page.js
@@ -63,7 +63,7 @@ function BannerJunior({ onToggleResults, showResults, onToggleQuiz2Results, show
   const MobileNavigation = dynamic(() => import('../Button/button.jsx'), { ssr: false });
 
   return (
-    <div className="w-screen">
+    <div className="w-screen max-md:pb-14">
       <div className="w-full py-8 md:text-3xl text-xl font-bold text-white text-center bg-gradient-to-r from-[#2f87aa] to-green-300">{t('careerAssesment')}</div>
       <div className="p-4">
         <div className="mt-8 md:flex hidden justify-evenly gap-10 w-full">
@@ -145,6 +145,7 @@ function BannerJunior({ onToggleResults, showResults, onToggleQuiz2Results, show
             className="pb-12"
           >
             {/* Personality Test - Mobile */}
+            {!getQuizStatus(1).isCompleted && (
             <SwiperSlide>
               <div className="pt-3 p-[1px] rounded-lg w-full" style={{
                 backgroundImage: `linear-gradient(to right, #3294bb, #3294bb)`,
@@ -163,20 +164,21 @@ function BannerJunior({ onToggleResults, showResults, onToggleQuiz2Results, show
                     </p>
                   </div>
                   <div className="flex justify-center items-center p-4 mt-auto">
-                    {!getQuizStatus(1).isCompleted ? (
+                    {/* {!getQuizStatus(1).isCompleted ? ( */}
                       <Link
                         href="/quiz-section-kids/1"
                         className="hover:cursor-pointer bg-gradient-to-r from-[#2f87aa] to-green-400 p-3 rounded-full w-40"
                       >
                         <p className="text-white font-semibold text-lg text-center">{t('takeTest')}</p>
                       </Link>
-                    ) : (
+                    {/* ) : (
                       <p className="text-white font-semibold bg-gradient-to-r from-[#2f87aa] to-green-400 text-lg text-center opacity-50 p-3 rounded-full w-40">{t('takeTest')}</p>
-                    )}
+                    )} */}
                   </div>
                 </div>
               </div>
             </SwiperSlide>
+            )}
 
             {/* Interest Test - Mobile */}
             <SwiperSlide>
@@ -214,6 +216,7 @@ function BannerJunior({ onToggleResults, showResults, onToggleQuiz2Results, show
           </Swiper>
 
           {/* Custom pagination dots */}
+          {!getQuizStatus(1).isCompleted &&(
           <div className="flex justify-center space-x-2 gap-2 mt-4 mb-16">
             {paginationDots.map((_, index) => (
               <div
@@ -223,6 +226,7 @@ function BannerJunior({ onToggleResults, showResults, onToggleQuiz2Results, show
               ></div>
             ))}
           </div>
+          )}
         </div>
       </div>
     </div>
