cf49d0de1b7907362a0170745ae474848292899a
doutyacodes <doutyacode@gmail.com>
2025-09-03 15:53:42 +0530

    updates - clusters

--- utils/index.js
+++ utils/index.js
@@ -1,20 +1,20 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "xortcut",
-  password:'',
-  port:'3306'
-});
-
 // const connection = await mysql.createConnection({
-//   host: "68.178.163.247",
-//   user: "devuser_quiz_project_user",
-//   database: "devuser_quiz_project",
-//   password:'Wowfy#user',
-//   port:'3306',
+//   host: "localhost",
+//   user: "root",
+//   database: "xortcut",
+//   password:'',
+//   port:'3306'
 // });
+
+const connection = await mysql.createConnection({
+  host: "68.178.163.247",
+  user: "devuser_quiz_project_user",
+  database: "devuser_quiz_project",
+  password:'Wowfy#user',
+  port:'3306',
+});
 
 export const db = drizzle(connection);
