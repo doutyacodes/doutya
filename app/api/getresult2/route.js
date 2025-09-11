56b6ef93f25c39210073735d5fce6e4bf797c7a8
doutyacodes <doutyacode@gmail.com>
2025-09-11 17:42:58 +0530

    updates - bg-fx-1

--- app/api/getresult2/route.js
+++ app/api/getresult2/route.js
@@ -19,7 +19,7 @@ const languageOptions = {
   mal: "in malayalam",
   tam: "in Tamil",
 };
-export const maxDuration = 60; // This function can run for a maximum of 5 seconds
+export const maxDuration = 300; // This function can run for a maximum of 5 seconds
 export const dynamic = "force-dynamic";
 
 export async function GET(req) {
